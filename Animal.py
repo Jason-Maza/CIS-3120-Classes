9f6ed486efb9d143dcd5109b652a46e891dedae1
Jason Maza <jason.j.maza@gmail.com>
2025-02-18 19:48:43 -0500

    Testing if synced

--- Animal.py
+++ Animal.py
@@ -1,7 +1,24 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name, food): 
+        self._name = name
+        self.__food = food
+        print("hello, I am", self._name)
 
-    def talk(self):
-        print("hi")
+        
+def food(self):
+        self.__food = food
+        print(f"crunch{self.__food}")
+    
+    
+    
+    def sleep:
+        print("zzzz")
+    
+    
+    def drink(self
+    
+    def hunt
+
+
+        
 
