7276f6a5c8d73554d5714a984dfbea0d86259e19
Jason Maza <jason.j.maza@gmail.com>
2025-02-20 21:46:28 -0500

    Updated Final File

--- Animal.py
+++ Animal.py
@@ -1,23 +1,17 @@
 class Animal:
-    def __init__(self, name, food, sleep, drink, hunt):  
+    def __init__(self, name, food, sleep, drink, hunt, talk):  
         self.__name = name
         self.__food = food
         self.__sleep = sleep
         self.__drink = drink
         self.__hunt = hunt
+        self.__talk = talk
         print("hello, I am", self.__name)
-        
-    def food(self):
         print("I Crunch", self.__food)
-    
-    def sleep(self):
-       print("I sleep", self.__sleep)
-
-    def drink(self):
-       print("I slurp", self.__drink)
-    
-    def hunt(self):
-       print("I Hunt", self.__hunt)
+        print("I sleep", self.__sleep)
+        print("I slurp", self.__drink)
+        print("I Hunt", self.__hunt)
+        print("I communicate by", self.__talk)
 
 
 
